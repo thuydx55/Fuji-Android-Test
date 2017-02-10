144c41d5ebef9d0b063438aaea50170bd6fc67f7
Nick <thuydx55vnu@gmail.com>
2017-02-10 14:10:23 +0700

    remove unused code

--- app/src/main/java/com/fuji/platform/MainActivity.java
+++ app/src/main/java/com/fuji/platform/MainActivity.java
@@ -97,15 +97,4 @@ public class MainActivity extends AppCompatActivity {
         mBtnPayment.setEnabled(FujiSDK.Instance.isLoggedIn());
         mBtnUserInfo.setEnabled(FujiSDK.Instance.isLoggedIn());
     }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
 }
